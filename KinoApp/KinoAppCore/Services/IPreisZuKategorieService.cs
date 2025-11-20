d8d79a86efa03a3df50369a5615f536691585998
Rafael131203 <rafaelulmer@gmail.com>
2025-11-20 23:39:09 +0100

    Zwischenstand

---
+++ KinoApp/KinoAppCore/Services/IPreisZuKategorieService.cs
@@ -0,0 +1,18 @@
+﻿using KinoAppDB.Entities;
+using KinoAppShared.DTOs.Kinosaal;
+using KinoAppShared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinoAppCore.Services
+{
+    public interface IPreisZuKategorieService
+    {
+        Task<PreisZuKategorieEntity?> GetPreisAsync(SitzreihenKategorie kategorie, CancellationToken ct);
+        Task<PreisZuKategorieEntity> SetPreisAsync(SetPreisDTO setPreisDTO, CancellationToken ct);
+    }
+
+}
---
+++ KinoApp/KinoAppDB/Entities/PreisZuKategorieEntity.cs
@@ -0,0 +1,16 @@
+﻿using KinoAppShared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinoAppDB.Entities
+{
+    public class PreisZuKategorieEntity
+    {
+        public long Id { get; set; }
+        public SitzreihenKategorie Kategorie { get; set; }
+        public decimal Preis {  get; set; }
+    }
+}
